3b9c15d00106f143b69257cc7e839039d064acfe
Ahmed Lakhani <>
2022-09-22 16:21:30 +0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './component/Layout';
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route  path='/' element={<Layout />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Layout/index.js
@@ -0,0 +1,16 @@
+import Sidebar from '../Sidebar';
+import './index.scss'
+
+const Layout = () => {
+    return (
+        <div className='App'>
+            <Sidebar />
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Layout;
